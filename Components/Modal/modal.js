691cbf2c5fda6507493fd2c5e4c0e3bbae8202ef
Parul <45730946+Parul1999@users.noreply.github.com>
2022-02-20 18:05:45 +0530

    Feat: Silder , Modal and Grid

---
+++ Components/Modal/modal.js
@@ -0,0 +1,30 @@
+
+const modal = document.querySelector(".modal");
+const modal2 = document.querySelector(".type2");
+
+const openModal=(clas)=>
+{
+    console.log(modal.classList[0])
+    if(clas == modal.classList[0]){
+    modal.style.cssText = `
+    display: flex; 
+    flex-wrap:wrap;
+    `;
+    }
+    else if(clas == modal2.classList[1])
+    {
+    modal2.style.cssText = `
+    display: flex; 
+    flex-wrap:wrap;
+    `;
+    }
+
+}
+
+const onClose=()=>
+{
+modal.style.display ="none"
+modal2.style.display="none"
+}
+
+openModal.addEventListener("click",openModal)
